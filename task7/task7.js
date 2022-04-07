9bb7a26e0f53aa3850317ef7d4f6c352135865b9
mithup15 <96760114+mithup15@users.noreply.github.com>
2022-04-07 10:48:54 +0530

    Update task7.js

--- task7/task7.js
+++ task7/task7.js
@@ -1,11 +1,15 @@
-function searchFun() {
+function searchFun()
+{
     var key = document.querySelector("#search").value.toUpperCase();
     var myFood = document.querySelectorAll(".food");
-    for (let i = 0; i < myFood.length; i++) {
+    for (let i = 0; i < myFood.length; i++) 
+    {
         var inItems = myFood[i].querySelector("h2").innerText.toUpperCase();
-        if (inItems.indexOf(key) > -1) {
+        if (inItems.indexOf(key) > -1)
+        {
             myFood[i].style.display = ""
-        } else {
+        } else 
+        {
             myFood[i].style.display = "none"
         }
     }
